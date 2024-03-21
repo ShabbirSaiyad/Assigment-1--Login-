2df2c9b6b455d710d3eec41479cb4613c126b6e3
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2024-03-21 11:33:42 +0530

    Some login stuff finished till now

---
+++ login.js
@@ -0,0 +1,28 @@
+
+function setCookies(username, password,expirydays){
+    const d = new Date();
+    d.setTime(d.getTime() + (expirydays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = "username"+  "=" + username +"password" + "=" + password + ";" + expires + ";"
+    console.log(document.cookie);
+}
+
+const myForm = document.querySelector('form');
+myForm.addEventListener('submit', function(event) {
+    let username = document.getElementById('username');
+    let user = username.value;
+    let password = document.getElementById('password');
+    let pass = password.value;
+   
+    // console.log(username);
+    // console.log(password);
+    // console.log('Form submitted!');
+    setCookies(user,pass,3);
+    alert("Form data submitted successfully");
+    username.value="";
+    password.value="";
+    window.location.href="/dashboard.html";
+
+    event.preventDefault(); 
+});
+
