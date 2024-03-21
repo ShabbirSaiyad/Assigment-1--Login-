c23adc1119dd8db99db4fb31822d8eafaf073205
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2024-03-21 11:57:05 +0530

    link update

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ myForm.addEventListener('submit', function(event) {
     alert("Form data submitted successfully");
     username.value="";
     password.value="";
-    document.location.href="https://www.google.com/";
+    document.location.href="https://google.com";
 
     event.preventDefault(); 
 });
